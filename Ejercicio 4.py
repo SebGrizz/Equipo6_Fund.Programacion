d6045d86b18cfd327ad981a2f3037d1026c333cb
Marcelo Arevalo <119123158+018452@users.noreply.github.com>
2022-12-10 11:13:09 -0500

    Add files via upload

---
+++ Ejercicio 4.py
@@ -0,0 +1,14 @@
+
+if __name__ == '__main__':
+	print("Lista de platos")
+	platos = str()
+	platos = [str() for ind0 in range(5)]
+	platos[1] = "Ceviche"
+	platos[2] = "Arroz con mariscos"
+	platos[3] = "Parihuela"
+	platos[4] = "Causa acevichada"
+	print("Plato: ",platos[4])
+	print("Plato: ",platos[3])
+	print("Plato: ",platos[2])
+	print("Plato: ",platos[1])
+
