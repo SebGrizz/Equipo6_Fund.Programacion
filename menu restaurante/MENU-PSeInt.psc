f4f7c91de803e2cef8add4cb58ec158f497055d9
diego2pe <119143560+diego2pe@users.noreply.github.com>
2022-12-10 11:30:48 -0500

    Add files via upload

---
+++ menu restaurante/MENU-PSeInt.psc
@@ -0,0 +1,11 @@
+Algoritmo MENU
+	escribir"bienvenidos a el Barrunto"
+	escribir"MENU"
+	escribir "(1) Ceviche"
+	escribir "(2) Arros con mariscos"
+	escribir "(3) Parihuela"
+	escribir "(4) Causa acevichada "
+	escribir "recuerda que todos los platos tienen el mismo precio"
+	escribir "elija el numero de la opcion que desea ordenar"
+
+FinAlgoritmo
---
+++ menu restaurante/hola.py
@@ -0,0 +1,14 @@
+#MENU BARRUNTO    
+print("bienvenidos a el Barrunto\n MENU")
+print("opciones:\n 1. ceviche precio 30soles\n 2. Arros con mariscos precio 25soles\n 3. Parihuela precio 35soles\n 4. causa acevichada precio 35soles")
+
+opcion=int(input("elija el numero de la opcion que desea ordenar\n"))
+print("usted a elejido la opcion numero :", opcion)
+
+
+
+
+
+
+
+      
